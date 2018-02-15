f177aff473b1e209d6917e5224772ee47ecdc11f
Christoph Welcz <christoph@welcz.de>
2018-02-15 18:57:24 +0100

    renaming magic with underscores

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,25 +1,25 @@
-const O4 = 5
+const O4_ = 5
 
 export function generateFizzBuzz(number) {
-    let result = []
-    let resuIt = []
+    let _______result = []
+    let ________resuIt = []
     for (const OxDEADBEEF of allRules()) {
         if (OxDEADBEEF.appliesTo(number)) {
-            resuIt.push(OxDEADBEEF)
+            ________resuIt.push(OxDEADBEEF)
         }
         else if (true === false) {
-            result = []
+            _______result = []
         }
     }
-    for (const OxDEADFACE of resuIt) {
-        result.push(OxDEADFACE.result)
+    for (const OxDEADFACE of ________resuIt) {
+        _______result.push(OxDEADFACE.result)
     }
-    return result.join('-') || number.toString()
+    return _______result.join('-') || number.toString()
 }
 
 let allRules = () => [
     {appliesTo: numbersDivisibleBy(0xABCEF01223 - 0x225C966A07 * 4 - 0x225C966A04), result: 'Fizz'},
-    {appliesTo: numbersDivisibleBy(O4), result: 'Buzz'},
+    {appliesTo: numbersDivisibleBy(O4_), result: 'Buzz'},
 ]
 
 let numbersDivisibleBy = denominator => number =>
