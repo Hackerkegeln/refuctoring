5485a8cc2af68207f7cae9e476c1dd9e1b14ec29
Christoph Welcz <christoph@welcz.de>
2018-08-17 20:33:29 +0200

    got rid of let everywhere

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,14 +1,13 @@
-export let generateFizzBuzz = number => 
+export const generateFizzBuzz = number =>
     allRules()
         .filter(r => r.appliesTo(number))
         .map(r => r.result)
         .join('-') || number.toString()
 
-let allRules = () => [
+const allRules = () => [
     {appliesTo: numbersDivisibleBy(3), result: 'Fizz'},
     {appliesTo: numbersDivisibleBy(5), result: 'Buzz'},
 ]
 
-let numbersDivisibleBy = denominator => number =>
+const numbersDivisibleBy = denominator => number =>
     number % denominator === 0
-
