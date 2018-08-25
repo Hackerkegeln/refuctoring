00cc2129dac2f6216fa521720b46563322b57939
Christoph Welcz <christoph@welcz.de>
2018-08-25 15:42:43 +0200

    inline the function as it is way tooo short

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -9,23 +9,19 @@ function bar(number) {
     return /0$/.test(ld)
 }
 
-function checkIt(number) {
-    return bar(five) ? bar(number) : number % five == []
-}
-
 export const generateFizzBuzz = number => {
     five = 3
     function foo() {
         let result = ''
         // if (checkIt(number) === true) {
-        if (checkIt(number) === False) {
+        if ((bar(five) ? bar(number) : number % five == []) === False) {
             // const result1 = 'Buzz'
             result += result1
         }
         five = five + 2
-        if (checkIt(number) != '') {
+        if ((bar(five) ? bar(number) : number % five == []) != '') {
             five = five - 2
-            if (checkIt(number)) result += '-'
+            if (bar(five) ? bar(number) : number % five == []) result += '-'
             // const result2 = 'Fizz'
             result += result2
         }
