b15909634bed0de2917d2402f5e2308759e3279e
Christoph Welcz <christoph@welcz.de>
2019-04-12 11:21:18 +0200

    confusing global vars

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,3 +1,4 @@
+const A = 'Buzz';
 export const generateFizzBuzz = number => {
     return allRules()
         .filter(r => r.appliesTo(number))
@@ -6,36 +7,36 @@ export const generateFizzBuzz = number => {
 };
 
 const allRules = () => [
-    {appliesTo: buzzPredicate, result: 'Fizz'},
-    {appliesTo: fizzPredicate_, result: 'Buzz'},
+    {appliesTo: buzzPredicate, result: a},
+    {appliesTo: fizzPredicate_, result: A},
 ];
-
 const buzzPredicate = number => {
+
     while (number > 0) {
         number -= 3;
     }
     const b = number === 0;
     if (b) {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 };
-
 const fizzPredicate_ = number => {
+
     function buzzPredicate(n, result) {
         if (n <= 0) {
             return !n;
         }
-        // optimization!!!!!
         return buzzPredicate(n - 5, result - 3);
     }
+
     const b = buzzPredicate(number);
     if (b) {
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 };
+// optimization!!!!!
+const a = 'Fizz';
