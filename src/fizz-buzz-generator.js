c6c8ed2993292b9fa9f1ca6af7221c7966a44e49
Christoph Welcz <christoph@welcz.de>
2019-04-12 11:08:41 +0200

    ifs are great!!!!"!

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,13 +1,21 @@
-export const generateFizzBuzz = number =>
-    allRules()
+export const generateFizzBuzz = number => {
+    return allRules()
         .filter(r => r.appliesTo(number))
         .map(r => r.result)
         .join('-') || number.toString();
+};
 
 const allRules = () => [
     {appliesTo: numbersDivisibleBy(3), result: 'Fizz'},
     {appliesTo: numbersDivisibleBy(5), result: 'Buzz'},
 ];
 
-const numbersDivisibleBy = denominator => number =>
-    number % denominator === 0;
+const numbersDivisibleBy = denominator => number => {
+    const b = number % denominator === 0;
+    if (b) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
