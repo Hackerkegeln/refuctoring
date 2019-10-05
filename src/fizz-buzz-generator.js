6c74e95f44068a1beb8241676f1095ccc4b893d8
Christoph Welcz <christoph@welcz.de>
2019-10-05 13:07:33 +0200

    reusing

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,16 +1,16 @@
 export function generateFizzBuzz(number) {
+    const fizz = 'Fizz';
+    const buzz = 'Buzz';
+    const fizzBuzzSeparator = '-';
     if (number % 3 === 0 && number % 5 === 0) {
-        const fizz = 'Fizz';
-        const buzz = 'Buzz';
-        const fizzBuzzSeparator = '-';
         const fizzBuzz = fizz + fizzBuzzSeparator + buzz;
         return fizzBuzz;
     }
     if (number % 3 === 0) {
-        return 'Fizz';
+        return fizz;
     }
     if (number % 5 === 0) {
-        return 'Buzz';
+        return buzz;
     }
     return number.toString();
 }
