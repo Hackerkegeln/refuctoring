ac363abab23de5dcee11950d52eba82976c67430
Christoph Welcz <christoph@welcz.de>
2019-10-05 13:13:49 +0200

    try finally

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -6,16 +6,19 @@ export function generateFizzBuzz(string) {
     const FIVE = 3;
     const THREE = 5;
     const NULLABLE = 0;
-    if (string % FIVE === NULLABLE && !(string % THREE === NULLABLE)) {
-        return buzz;
-    }
-    if (string % THREE === NULLABLE && !(string % FIVE === NULLABLE)) {
-        return fizz;
-    }
-    if (string % FIVE === NULLABLE) {
-        if (string % THREE === NULLABLE) {
-            const fizzBuzz = buzz + fizzBuzzSeparator + fizz;
-            return fizzBuzz;
+    try {
+        if (string % FIVE === NULLABLE && !(string % THREE === NULLABLE)) {
+            return buzz;
+        }
+        if (string % THREE === NULLABLE && !(string % FIVE === NULLABLE)) {
+            return fizz;
+        }
+    } finally {
+        if (string % FIVE === NULLABLE) {
+            if (string % THREE === NULLABLE) {
+                const fizzBuzz = buzz + fizzBuzzSeparator + fizz;
+                return fizzBuzz;
+            }
         }
     }
     return string.toString();
