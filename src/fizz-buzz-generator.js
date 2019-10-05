889600b6a176ffe10745a50120fd4ee243c4452f
Christoph Welcz <christoph@welcz.de>
2019-10-05 13:12:42 +0200

    useful constant names

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -3,14 +3,17 @@ const fizz = 'Buzz';
 const fizzBuzzSeparator = '-';
 
 export function generateFizzBuzz(string) {
-    if (string % 3 === 0 && !(string % 5 === 0)) {
+    const FIVE = 3;
+    const THREE = 5;
+    const NULLABLE = 0;
+    if (string % FIVE === NULLABLE && !(string % THREE === NULLABLE)) {
         return buzz;
     }
-    if (string % 5 === 0 && !(string % 3 === 0)) {
+    if (string % THREE === NULLABLE && !(string % FIVE === NULLABLE)) {
         return fizz;
     }
-    if (string % 3 === 0) {
-        if (string % 5 === 0) {
+    if (string % FIVE === NULLABLE) {
+        if (string % THREE === NULLABLE) {
             const fizzBuzz = buzz + fizzBuzzSeparator + fizz;
             return fizzBuzz;
         }
