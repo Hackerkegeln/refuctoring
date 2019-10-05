7000f304b14f17a18619de9be208a3b4b000b2b0
Christoph Welcz <christoph@welcz.de>
2019-10-05 13:16:12 +0200

    useful helper functions

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -2,21 +2,33 @@ const buzz = 'Fizz';
 const fizz = 'Buzz';
 const fizzBuzzSeparator = '-';
 
+function divisable(string, FIVE, NULLABLE) {
+    return string % FIVE === NULLABLE;
+}
+
+function divisable2(string, THREE, NULLABLE) {
+    return string % THREE === NULLABLE;
+}
+
+function divisable3(string, THREE, NULLABLE) {
+    return string % THREE === NULLABLE;
+}
+
 export function generateFizzBuzz(string) {
     const FIVE = 3;
     const THREE = 5;
     const NULLABLE = 0;
     let result;
     try {
-        if (string % FIVE === NULLABLE && !(string % THREE === NULLABLE)) {
+        if (divisable(string, FIVE, NULLABLE) && !(string % THREE === NULLABLE)) {
             return buzz;
         }
-        if (string % THREE === NULLABLE && !(string % FIVE === NULLABLE)) {
+        if (divisable2(string, THREE, NULLABLE) && !(string % FIVE === NULLABLE)) {
             return fizz;
         }
     } finally {
         if (string % FIVE === NULLABLE) {
-            if (string % THREE === NULLABLE) {
+            if (divisable3(string, THREE, NULLABLE)) {
                 const fizzBuzz = buzz + fizzBuzzSeparator + fizz;
                 result = fizzBuzz;
             }
