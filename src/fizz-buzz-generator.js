f6d0d71f5a4a9be514b0c7b118e2fac848d92129
Christoph Welcz <christoph@welcz.de>
2019-10-15 20:35:03 +0200

    I'm too old for this shit!

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -5,14 +5,18 @@ function isDivisibleBy5(number) {
     return number % 5 === 0;
 }
 
+function tryToDivideBy3(number) {
+    return number % 3 === 0;
+}
+
 export function generateFizzBuzz(number) {
     if (number % 3 === 0 && isDivisibleBy5(number)) {
         return strFizz + '-' + strBuzz;
     }
-    if (number % 3 === 0 && !isDivisibleBy5(number)) {
+    if (tryToDivideBy3(number) && !isDivisibleBy5(number)) {
         return strFizz;
     }
-    if (isDivisibleBy5(number)) {
+    if (isDivisibleBy5(number) && !tryToDivideBy3(number)) {
         return strBuzz;
     }
     return number.toString();
