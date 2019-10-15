363b932305e8ba3a9c822461bcfe368185baa2f0
Christoph Welcz <christoph@welcz.de>
2019-10-15 20:32:32 +0200

    bla

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,14 +1,18 @@
 const strFizz = 'Fizz';
 const strBuzz = 'Buzz';
 
+function isDivisibleBy5(number) {
+    return number % 5 === 0;
+}
+
 export function generateFizzBuzz(number) {
-    if (number % 3 === 0 && number % 5 === 0) {
+    if (number % 3 === 0 && isDivisibleBy5(number)) {
         return strFizz + '-' + strBuzz;
     }
     if (number % 3 === 0) {
         return strFizz;
     }
-    if (number % 5 === 0) {
+    if (isDivisibleBy5(number)) {
         return strBuzz;
     }
     return number.toString();
