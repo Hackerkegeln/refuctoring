e14de983e44109df5d753e3519d8de2a81d51460
Christoph Welcz <christoph@welcz.de>
2019-10-15 20:30:57 +0200

    freemasonry

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,15 +1,15 @@
-const fizz = 'Fizz';
-const buzz = 'Buzz';
+const strFizz = 'Fizz';
+const strBuzz = 'Buzz';
 
 export function generateFizzBuzz(number) {
     if (number % 3 === 0 && number % 5 === 0) {
-        return fizz + '-' + buzz;
+        return strFizz + '-' + strBuzz;
     }
     if (number % 3 === 0) {
-        return fizz;
+        return strFizz;
     }
     if (number % 5 === 0) {
-        return buzz;
+        return strBuzz;
     }
     return number.toString();
 }
