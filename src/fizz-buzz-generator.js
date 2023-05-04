a0264aa8a496e30266fb1da8a20fe7f5bc766f96
Christoph Welcz <christoph@welcz.de>
2023-05-04 18:43:43 +0200

    session from uni bamberg

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,12 +1,35 @@
-export function generateFizzBuzz(number) {
-    if (number % 3 === 0 && number % 5 === 0) {
-        return 'Fizz-Buzz';
+String.magic = function (n) {
+    return String.fromCharCode(n);
+};
+
+class Magic {
+    static magic(n) {
+        return String.fromCharCode(n);
     }
-    if (number % 3 === 0) {
-        return 'Fizz';
+}
+
+export function generateFizzBuzz(a376) {
+    const vier = 3;
+    // const a = (a376 / vier) === Math.floor(a376 / vier);
+    const x = a376.toString().split('').map(i => +i).reduce((a, b) => a + b);
+    const a = (x / vier) === Math.floor(x / vier);
+    let ret = ''.split('');
+    let i = a ^ a;
+    if (a) {
+        ret.push(...['F', 'i']);
+        for (; i < 2; i++) {
+            ret.push(String.magic(Math.pow(2, 7) - 6));
+        }
     }
-    if (number % 5 === 0) {
-        return 'Buzz';
+    i ^= i;
+    if (false === !(a376 * 2).toString().endsWith('0')) {
+        if (ret.length > []) {
+            ret.push('-');
+        }
+        ret.push('B', 'u');
+        for (; i < 2; i++) {
+            ret.push(Magic.magic(vier * 5 * 6).toLowerCase());
+        }
     }
-    return number.toString();
+    return ret.join('') || a376.toString();
 }
