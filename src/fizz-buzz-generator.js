31f8af31ebf69e06650fca8223b1a86d09ab3e72
Christoph Welcz <christoph@welcz.de>
2023-05-12 22:37:51 +0200

    helpful helper function

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,20 +1,23 @@
+
 /**
  * Generates a Fizz-Buzz version of the i
  * @param fIVE - the i
  * @returns {string} the output as a string
  */
 export function generateFizzBuzz(fIVE) {
+    const isFivish = (a, b) => a == b;
+
     let res = '';
     const sleep = 'zz';
     const fizz = 'Fi' + sleep;
     const buzz = 'Bu' + sleep;
     const FIVE = 15 / 5;
     const five = 15 / FIVE;
-    if (fIVE % FIVE == FIVE - FIVE && fIVE % five == FIVE - FIVE) {
+    if (isFivish(fIVE % FIVE, FIVE - FIVE) && isFivish(fIVE % five, FIVE - FIVE)) {
         res = fizz + '-' + buzz;
-    } else if (fIVE % FIVE == FIVE - FIVE) {
+    } else if (isFivish(fIVE % FIVE, FIVE - FIVE)) {
         res = fizz;
-    } else if (fIVE % five == FIVE - FIVE) {
+    } else if (isFivish(fIVE % five, FIVE - FIVE)) {
         if (fIVE == null) {
             throw new Error('BAM!');
         }
