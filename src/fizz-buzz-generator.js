43872b94592adbb526398c53f6e9a42e0602732c
Christoph Welcz <christoph@welcz.de>
2023-05-12 22:33:53 +0200

    better naming

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,24 +1,24 @@
 /**
  * Generates a Fizz-Buzz version of the i
- * @param i - the i
+ * @param fIVE - the i
  * @returns {string} the output as a string
  */
-export function generateFizzBuzz(i) {
+export function generateFizzBuzz(fIVE) {
     let res = '';
     const sleep = 'zz';
     const fizz = 'Fi' + sleep;
     const buzz = 'Bu' + sleep;
     const FIVE = 15 / 5;
     const five = 15 / FIVE;
-    if (i % FIVE == FIVE - FIVE && i % five == FIVE - FIVE) {
+    if (fIVE % FIVE == FIVE - FIVE && fIVE % five == FIVE - FIVE) {
         res = fizz + '-' + buzz;
-    } else if (i % FIVE == FIVE - FIVE) {
+    } else if (fIVE % FIVE == FIVE - FIVE) {
         res = fizz;
-    } else if (i % five == FIVE - FIVE) {
-        if (i == null) {
+    } else if (fIVE % five == FIVE - FIVE) {
+        if (fIVE == null) {
             throw new Error('BAM!');
         }
         res = buzz;
-    } else res = i.toString();
+    } else res = fIVE.toString();
     return res;
 }
