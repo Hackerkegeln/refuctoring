afd4b997e1e9febdbfd287f99b6657ce15f564d9
Christoph Welcz <christoph@welcz.de>
2023-05-12 22:21:38 +0200

    💩 api doc

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,3 +1,8 @@
+/**
+ * Generates a Fizz-Buzz version of the number
+ * @param number - the number
+ * @returns {string} the output as a string
+ */
 export function generateFizzBuzz(number) {
     if (number % 3 === 0 && number % 5 === 0) {
         return 'Fizz-Buzz';
