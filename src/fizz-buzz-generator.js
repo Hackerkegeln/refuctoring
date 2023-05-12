d2d7c8bc05cfcea78a98833310d5376375f221fa
Christoph Welcz <christoph@welcz.de>
2023-05-12 22:42:42 +0200

    yes/no are not clear enough

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -5,7 +5,7 @@
  * @returns {string} the output as a string
  */
 export function generateFizzBuzz(fIVE) {
-    const isFivish2 = (a, b) => a == b ? 'yes' : 'no';
+    const isFivish2 = (a, b) => a == b ? 'isFivish' : 'isNotFivish';
 
     let res = '';
     const sleep = 'zz';
@@ -13,12 +13,12 @@ export function generateFizzBuzz(fIVE) {
     const buzz = 'Bu' + sleep;
     const FIVE = 15 / 5;
     const five = 15 / FIVE;
-    if (isFivish2(fIVE % FIVE, FIVE - FIVE) == 'yes' && isFivish2(fIVE % five, FIVE - FIVE) == 'yes') {
+    if (isFivish2(fIVE % FIVE, FIVE - FIVE) == 'isFivish' && isFivish2(fIVE % five, FIVE - FIVE) == 'isFivish') {
         res = fizz + '-' + buzz;
-    } else if (isFivish2(fIVE % FIVE, FIVE - FIVE) == 'yes') {
+    } else if (isFivish2(fIVE % FIVE, FIVE - FIVE) == 'isFivish') {
         res = fizz;
-    } else if (isFivish2(fIVE % five, FIVE - FIVE) == 'yes') {
-        if (isFivish2(fIVE, null) == 'yes') {
+    } else if (isFivish2(fIVE % five, FIVE - FIVE) == 'isFivish') {
+        if (isFivish2(fIVE, null) == 'isFivish') {
             throw new Error('BAM!');
         }
         res = buzz;
