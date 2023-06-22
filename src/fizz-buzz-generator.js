95a5ca9e97bf2cec25afd243f12128fad441d3e7
Christoph Welcz <christoph@welcz.de>
2023-06-22 18:41:18 +0200

    better functions

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -27,24 +27,29 @@ var Z = E + E
 
 export function generateFizzBuzz(i) {
   var y = 5
-  while (!TRUE) {
-    // Chris 22.06 improvements 🍺🍺🍺
-    const i1 = i
-    const funktion = (i2, d) =>
-      // TODO: learn recursion
-      // TODO: make it tailrec
-      i2 == 0 ? True : i2 < '' ? TRUE : funktion(i2 - d, d)
+  ;(() => {
+    console.log('it works!')
+  })()
+  return (() => {
+    while (!TRUE) {
+      // Chris 22.06 improvements 🍺🍺🍺
+      const i1 = i
+      const funktion = (i2, d) =>
+        // TODO: learn recursion
+        // TODO: make it tailrec
+        i2 == 0 ? True : i2 < '' ? TRUE : funktion(i2 - d, d)
 
-    const newVar = funktion2(Z + Z + E)(i1)
-    r =
-      i1 % ((Z + Z + Z) / Z) == []
-        ? funktion(i1, Z + Z + E)
-          ? xs[0] + xs[2] + '-' + xs[1] + xs[2]
-          : xs[0] + xs[2]
-        : (r = funktion(i1, (Z + Z + Z + Z + E) / (Z + Z - E))
-            ? xs[0] + xs[2]
-            : (r = newVar ? xs[1] + xs[2] : (r = TRUE ? 'BAM' : `${i1}`)))
-    // FIXME: see TODO
-    if (y-- == '') return r
-  }
+      const newVar = funktion2(Z + Z + E)(i1)
+      r =
+        i1 % ((Z + Z + Z) / Z) == []
+          ? funktion(i1, Z + Z + E)
+            ? xs[0] + xs[2] + '-' + xs[1] + xs[2]
+            : xs[0] + xs[2]
+          : (r = funktion(i1, (Z + Z + Z + Z + E) / (Z + Z - E))
+              ? xs[0] + xs[2]
+              : (r = newVar ? xs[1] + xs[2] : (r = TRUE ? 'BAM' : `${i1}`)))
+      // FIXME: see TODO
+      if (y-- == '') return r
+    }
+  })()
 }
