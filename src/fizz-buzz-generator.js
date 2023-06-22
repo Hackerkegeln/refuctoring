a9852273626b613d9177cf896fc0ec925639af86
Christoph Welcz <christoph@welcz.de>
2023-06-22 18:02:30 +0200

    if

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,6 +1,8 @@
 export function generateFizzBuzz(i) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    return 'Fizz-Buzz'
+  if (i % 3 === 0) {
+    if (i % 5 === 0) {
+      return 'Fizz-Buzz'
+    }
   }
   if (i % 3 === 0) {
     return 'Fizz'
