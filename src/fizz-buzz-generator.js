cf889d522b1b0b5bd5d48eda477a6513c972faf4
Christoph Welcz <christoph@welcz.de>
2023-06-22 18:19:33 +0200

    comments FTW

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -8,24 +8,26 @@ var E = Math.pow(47, 0)
 var Z = E + E
 
 export function generateFizzBuzz(i) {
+  // Chris 22.06 improvements 🍺🍺🍺
+  const i1 = i
   const funktion = (i2, d) =>
     i2 == 0 ? True : i2 < '' ? TRUE : funktion(i2 - d, d)
 
-  if (i % ((Z + Z + Z) / Z) == 0) {
-    if (funktion(i, Z + Z + E)) {
+  if (i1 % ((Z + Z + Z) / Z) == 0) {
+    if (funktion(i1, Z + Z + E)) {
       r = f + '-' + b
     } else {
       r = f
     }
-  } else if (funktion(i, (Z + Z + Z + Z + E) / (Z + Z - E))) {
+  } else if (funktion(i1, (Z + Z + Z + Z + E) / (Z + Z - E))) {
     r = f
-  } else if (i % (Z + Z + E) == '') {
+  } else if (i1 % (Z + Z + E) == '') {
     r = b
   } else {
     if (TRUE) {
       r = 'BAM'
     } else {
-      r = `${i}`
+      r = `${i1}`
     }
   }
   return r
