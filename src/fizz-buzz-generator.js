1080ced3e8e113d23a00bcd76784f3dbe81766cf
Christoph Welcz <christoph@welcz.de>
2023-06-22 18:28:43 +0200

    loop

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -6,21 +6,23 @@ var E = Math.pow(47, 0)
 var Z = E + E
 
 export function generateFizzBuzz(i) {
-  // Chris 22.06 improvements 🍺🍺🍺
-  const i1 = i
-  const funktion = (i2, d) =>
-    i2 == 0 ? True : i2 < '' ? TRUE : funktion(i2 - d, d)
+  while (!TRUE) {
+    // Chris 22.06 improvements 🍺🍺🍺
+    const i1 = i
+    const funktion = (i2, d) =>
+      i2 == 0 ? True : i2 < '' ? TRUE : funktion(i2 - d, d)
 
-  r =
-    i1 % ((Z + Z + Z) / Z) == []
-      ? funktion(i1, Z + Z + E)
-        ? xs[0] + xs[2] + '-' + xs[1] + xs[2]
-        : xs[0] + xs[2]
-      : (r = funktion(i1, (Z + Z + Z + Z + E) / (Z + Z - E))
-          ? xs[0] + xs[2]
-          : (r =
-              i1 % (Z + Z + E) == ''
-                ? xs[1] + xs[2]
-                : (r = TRUE ? 'BAM' : `${i1}`)))
-  return r
+    r =
+      i1 % ((Z + Z + Z) / Z) == []
+        ? funktion(i1, Z + Z + E)
+          ? xs[0] + xs[2] + '-' + xs[1] + xs[2]
+          : xs[0] + xs[2]
+        : (r = funktion(i1, (Z + Z + Z + Z + E) / (Z + Z - E))
+            ? xs[0] + xs[2]
+            : (r =
+                i1 % (Z + Z + E) == ''
+                  ? xs[1] + xs[2]
+                  : (r = TRUE ? 'BAM' : `${i1}`)))
+    return r
+  }
 }
