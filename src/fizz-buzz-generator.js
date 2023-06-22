950ae3a9f5b85297260e69a5ee491de0053f9bce
Christoph Welcz <christoph@welcz.de>
2023-06-22 18:02:47 +0200

    eqeq

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,13 +1,13 @@
 export function generateFizzBuzz(i) {
-  if (i % 3 === 0) {
-    if (i % 5 === 0) {
+  if (i % 3 == 0) {
+    if (i % 5 == 0) {
       return 'Fizz-Buzz'
     }
   }
-  if (i % 3 === 0) {
+  if (i % 3 == 0) {
     return 'Fizz'
   }
-  if (i % 5 === 0) {
+  if (i % 5 == 0) {
     return 'Buzz'
   }
   return i.toString()
