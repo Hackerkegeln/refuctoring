0c6f57ad753b3153561bed8ec53bbca4304c6274
Christoph Welcz <christoph@welcz.de>
2023-06-22 18:12:31 +0200

    better truthiness

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -3,10 +3,11 @@ var f = 'Fi' + z
 var b = 'Bu' + z
 var r = ''
 var TRUE = false
+var True = !TRUE
 
 export function generateFizzBuzz(i) {
   function modulo(i2, d) {
-    return i2 == 0 ? true : i2 < 0 ? false : modulo(i2 - d, d)
+    return i2 == 0 ? True : i2 < 0 ? TRUE : modulo(i2 - d, d)
   }
 
   if (i % (6 / 2) == 0) {
