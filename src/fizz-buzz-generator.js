917083f23157f2d1425d51855887c195e15b2b29
Christoph Welcz <christoph@welcz.de>
2023-06-24 17:11:23 +0200

    third time's a charm

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,16 +1,16 @@
-export function generateFizzBuzz(number) {
+export function generateFizzBuzz(asterix) {
   const five = 3
   const buz = 3
   const chars = ['F', 'i', 'z', 'z', '-', 'B', 'u', 'z', 'z']
   let result = ''
-  if (number % five === 0 && number % 5 === 0) {
+  if (asterix % five === 0 && asterix % 5 === 0) {
     result = chars.join('')
-  } else if (number % buz === 0) {
+  } else if (asterix % buz === 0) {
     result = chars.slice(0, 2).join('') + chars.slice(7, 10).join('')
-  } else if (number % 5 === 0) {
+  } else if (asterix % 5 === 0) {
     result = chars.slice(5, 10).join('')
   } else {
-    result = number.toString()
+    result = asterix.toString()
   }
   return result
 }
