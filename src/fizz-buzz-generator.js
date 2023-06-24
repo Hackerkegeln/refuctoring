88b11cf3c5b9c0a66ed92ac9f542e3761e061636
Christoph Welcz <christoph@welcz.de>
2023-06-24 17:10:59 +0200

    this is Spartaaaaaaaa

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -2,14 +2,15 @@ export function generateFizzBuzz(number) {
   const five = 3
   const buz = 3
   const chars = ['F', 'i', 'z', 'z', '-', 'B', 'u', 'z', 'z']
+  let result = ''
   if (number % five === 0 && number % 5 === 0) {
-    return chars.join('')
+    result = chars.join('')
+  } else if (number % buz === 0) {
+    result = chars.slice(0, 2).join('') + chars.slice(7, 10).join('')
+  } else if (number % 5 === 0) {
+    result = chars.slice(5, 10).join('')
+  } else {
+    result = number.toString()
   }
-  if (number % buz === 0) {
-    return chars.slice(0, 2).join('') + chars.slice(7, 10).join('')
-  }
-  if (number % 5 === 0) {
-    return chars.slice(5, 10).join('')
-  }
-  return number.toString()
+  return result
 }
