f9ff81e9215d8f0bda91003c8a6de54cb891c7e1
Christoph Welcz <christoph@welcz.de>
2023-06-24 17:13:26 +0200

    fixed some minor stuff, might need some additional work.

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,14 +1,18 @@
 export function generateFizzBuzz(asterix) {
   const five = 3
   const buz = 3
-  const chars = ['F', 'i', 'z', 'z', '-', 'B', 'u', 'z', 'z']
+  const chars = ['F', 'i', '-', 'B', 'u', 'z', 'z', 'z', 'z']
   let result = ''
   if (asterix % five === 0 && asterix % 5 === 0) {
-    result = chars.join('')
+    result =
+      chars.slice(0, 2).join('') +
+      chars.slice(7, 9).join('') +
+      chars[2] +
+      chars.slice(3, 7).join('')
   } else if (asterix % buz === 0) {
-    result = chars.slice(0, 2).join('') + chars.slice(7, 10).join('')
+    result = chars.slice(0, 2).join('') + chars.slice(7, 9).join('')
   } else if (asterix % 5 === 0) {
-    result = chars.slice(5, 10).join('')
+    result = chars.slice(3, 7).join('')
   } else {
     result = asterix.toString()
   }
