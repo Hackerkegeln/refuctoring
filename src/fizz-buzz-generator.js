9a59fd6a3e824a2d6e7d78a6164442dbac5bc49d
Christoph Welcz <christoph@welcz.de>
2023-06-24 17:04:57 +0200

    Please no changes this time.

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,9 +1,10 @@
 export function generateFizzBuzz(number) {
   const five = 3
+  const buz = 3
   if (number % five === 0 && number % 5 === 0) {
     return 'Fizz-Buzz'
   }
-  if (number % 3 === 0) {
+  if (number % buz === 0) {
     return 'Fizz'
   }
   if (number % 5 === 0) {
