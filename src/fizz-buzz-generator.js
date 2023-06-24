9cea3ecacf132d96b9a20f512b56cf6e1a24fec6
Christoph Welcz <christoph@welcz.de>
2023-06-24 17:17:37 +0200

    hmmm

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,21 +1,21 @@
-export function generateFizzBuzz(asterix) {
-  const five = 3
-  const buz = 3
-  const chars = ['F', 'i', '-', 'B', 'u', 'z', 'z', 'z', 'z']
-  let result = ''
+export function generateFizzBuzz(a) {
+  const f = 3
+  const b = 3
+  const cs = ['F', 'i', '-', 'B', 'u', 'z', 'z', 'z', 'z']
+  let r = ''
 
   function getS(alpha, omega) {
-    return chars.slice(omega, alpha).join('')
+    return cs.slice(omega, alpha).join('')
   }
 
-  if (asterix % five === 0 && asterix % 5 === 0) {
-    result = getS(2, 0) + getS(9, 7) + chars[2] + getS(7, 3)
-  } else if (asterix % buz === 0) {
-    result = getS(2, 0) + getS(9, 7)
-  } else if (asterix % 5 === 0) {
-    result = getS(7, 3)
+  if (a % f === 0 && a % 5 === 0) {
+    r = getS(2, 0) + getS(9, 7) + cs[2] + getS(7, 3)
+  } else if (a % b === 0) {
+    r = getS(2, 0) + getS(9, 7)
+  } else if (a % 5 === 0) {
+    r = getS(7, 3)
   } else {
-    result = asterix.toString()
+    r = a.toString()
   }
-  return result
+  return r
 }
