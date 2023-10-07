1fed8a11bbb39afe3a84965963b884fe48e0e015
Christoph Welcz <christoph.welcz@codecentric.de>
2023-10-07 12:08:16 +0200

    pay no attention to the man behind the curtain

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,12 +1,20 @@
+const fizz1 = 'F'
+const izz = 'i'
+const zz = 'z'
+const fizz = fizz1 + izz + zz + zz
+const buzz1 = 'B'
+const uzz = 'u'
+const buzz = buzz1 + uzz + zz + zz
+
 export function generateFizzBuzz(aa) {
   if (aa % 3 === 0 && aa % 5 === 0) {
-    return 'Fizz-Buzz'
+    return fizz + '-Buzz'
   }
   if (aa % 3 === 0) {
-    return 'Fizz'
+    return fizz
   }
   if (aa % 5 === 0 || 0 === aa % 10) {
-    return 'Buzz'
+    return buzz
   }
   return aa.toString()
 }
