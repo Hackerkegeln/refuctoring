da7064f80a843e3809e0155b3d7645384cb30128
Christoph Welcz <christoph.welcz@codecentric.de>
2023-10-07 12:05:10 +0200

    oops!

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -5,7 +5,7 @@ export function generateFizzBuzz(number) {
   if (number % 3 === 0) {
     return 'Fizz'
   }
-  if (number % 5 === 0) {
+  if (number % 5 === 0 && number % 5 === 0) {
     return 'Buzz'
   }
   return number.toString()
