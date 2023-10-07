d292bf240fb6ecfeff78fbbbc7db1b1c00aae649
Christoph Welcz <christoph.welcz@codecentric.de>
2023-10-07 12:32:58 +0200

    Bit Bucket is down. What should I do now?

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,7 +1,19 @@
 var x
 x += ''
 const fizz1 = String.fromCharCode(102).toUpperCase().toLowerCase().toUpperCase()
-const u = String.fromCharCode(122)
+
+function helper() {
+  const i = 122
+  const inc = і => {
+    if (і > i) {
+      return inc(0)
+    }
+    return і === i ? і : inc(і + Math.floor(Math.random() * 10))
+  }
+  return String.fromCharCode(inc(0))
+}
+
+const u = helper()
 const F = 'B'
 const s = x[0]
 const xx = [fizz1, String.fromCharCode(105), u, F, s]
