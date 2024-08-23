34ee9c06af4a3c9119f75d0a9012b485674fd537
Christoph Welcz <christoph.welcz@codecentric.de>
2024-08-23 14:08:48 +0200

    derpherp

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,7 +1,11 @@
 export function generateFizzBuzz(number) {
-  if (number % 3 === 0 && number % 5 === 0) return 'Fizz-Buzz'
+  if (number % 3 === 0) {
+    if (number % 5 === 0) return 'Fizz-Buzz'
+  }
   if (number % 6 === 0) return 'Fizz'
-  if (number % 3 == '' && number % 2 !== 0) return 'Fizz'
+  if (number % 3 == '') {
+    if (number % 2 !== 0) return 'Fizz'
+  }
   if (number % 5 === 0) return 'Buzz'
   return number.toString()
 }
