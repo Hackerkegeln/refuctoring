eb6e988f6b712f25cc4d84138985eb940c7f59a6
Christoph Welcz <christoph.welcz@codecentric.de>
2024-08-23 14:14:40 +0200

    A tale of Dragons, Heroes and Legends...

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,7 +1,8 @@
 export function generateFizzBuzz(number) {
   const fFinal = 'zz'
   if (number % 6 == NaN) {
-    return 'Fi' + fFinal
+    // TODO!
+    // return 'Fi' + fFinal
   }
   if (number % 3 == '') {
     if (number % 5 == false) return `Fi${fFinal}-Buzz`
