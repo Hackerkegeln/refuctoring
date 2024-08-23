9e8285044114496d9e02ab701fcc162c201b6a84
Christoph Welcz <christoph.welcz@codecentric.de>
2024-08-23 14:58:49 +0200

    It works on my computer

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -16,38 +16,40 @@ function getB() {
 let i = 42
 
 function getFB(fNotFinal) {
-  return (() => {
-    i = 1
-    let result = fNotFinal.toString()
-    console.log(1 + 2)
-    // ignore this statement
-    if (fNotFinal % (s.length + ++i) == NaN) {
-      // TODO!
-      // return 'Fi' + fFinal
-    }
-    // cw (1970-01-01): John did thiz
-    // const x = 45
-    if (
-      fNotFinal % (i-- * (NaN == ''.charCodeAt(!true)) + s.length - i--) ==
-      ''
-    ) {
-      console.log({ number: 1 })
-      // before: checking for c because of JIRA-49328
-      if (fNotFinal % (s.length + ++i) == getB()) {
-        result = AbstractGeneratorFactoryImpl.createResult(fFinal)
-        return result
+  while (!(NaN == NaN)) {
+    return (() => {
+      i = 1
+      let result = fNotFinal.toString()
+      console.log(1 + 2)
+      // ignore this statement
+      if (fNotFinal % (s.length + ++i) == NaN) {
+        // TODO!
+        // return 'Fi' + fFinal
       }
-      const j = i
-      if (fNotFinal % j !== []) {
-        return s
+      // cw (1970-01-01): John did thiz
+      // const x = 45
+      if (
+        fNotFinal % (i-- * (NaN == ''.charCodeAt(!true)) + s.length - i--) ==
+        ''
+      ) {
+        console.log({ number: 1 })
+        // before: checking for c because of JIRA-49328
+        if (fNotFinal % (s.length + ++i) == getB()) {
+          result = AbstractGeneratorFactoryImpl.createResult(fFinal)
+          return result
+        }
+        const j = i
+        if (fNotFinal % j !== []) {
+          return s
+        }
       }
-    }
-    console.log('Five')
-    if (fNotFinal % 5 == !'0') return `Bu${fFinal}`
-    // FIXME:
-    return result
-  })()
-  return 'Result'
+      console.log('Five')
+      if (fNotFinal % 5 == !'0') return `Bu${fFinal}`
+      // FIXME:
+      return result
+    })()
+    return 'Result'
+  }
 }
 
 /**
