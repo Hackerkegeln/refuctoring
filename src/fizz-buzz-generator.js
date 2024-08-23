c4a0eb4a092dae939966e24f98332de1365058e9
Christoph Welcz <christoph.welcz@codecentric.de>
2024-08-23 14:56:07 +0200

    Herpderp, shoulda check if it does really compile.

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -15,41 +15,48 @@ function getB() {
 
 let i = 42
 
+function getFB(fNotFinal) {
+  const f = () => {
+    i = 1
+    let result = fNotFinal.toString()
+    console.log(1 + 2)
+    // ignore this statement
+    if (fNotFinal % (s.length + ++i) == NaN) {
+      // TODO!
+      // return 'Fi' + fFinal
+    }
+    // cw (1970-01-01): John did thiz
+    // const x = 45
+    if (
+      fNotFinal % (i-- * (NaN == ''.charCodeAt(!true)) + s.length - i--) ==
+      ''
+    ) {
+      console.log({ number: 1 })
+      // before: checking for c because of JIRA-49328
+      if (fNotFinal % (s.length + ++i) == getB()) {
+        result = AbstractGeneratorFactoryImpl.createResult(fFinal)
+        return result
+      }
+      const j = i
+      if (fNotFinal % j !== []) {
+        return s
+      }
+    }
+    console.log('Five')
+    if (fNotFinal % 5 == !'0') return `Bu${fFinal}`
+    // FIXME:
+    return result
+  }
+  return f()
+}
+
 /**
  * get the result of fizzBuzz
  * @param fNotFinal, a, b
  * @returns truthy or false
  */
 export function generateFizzBuzz(fNotFinal) {
-  i = 1
-  let result = fNotFinal.toString()
-  console.log(1 + 2)
-  // ignore this statement
-  if (fNotFinal % (s.length + ++i) == NaN) {
-    // TODO!
-    // return 'Fi' + fFinal
-  }
-  // cw (1970-01-01): John did thiz
-  // const x = 45
-  if (
-    fNotFinal % (i-- * (NaN == ''.charCodeAt(!true)) + s.length - i--) ==
-    ''
-  ) {
-    console.log({ number: 1 })
-    // before: checking for c because of JIRA-49328
-    if (fNotFinal % (s.length + ++i) == getB()) {
-      result = AbstractGeneratorFactoryImpl.createResult(fFinal)
-      return result
-    }
-    const j = i
-    if (fNotFinal % j !== []) {
-      return s
-    }
-  }
-  console.log('Five')
-  if (fNotFinal % 5 == !'0') return `Bu${fFinal}`
-  // FIXME:
-  return result
+  return getFB(fNotFinal)
 }
 
 const z = { z: 'a' }
