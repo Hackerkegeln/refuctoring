18f6d8a74eabe824a173acdc2089f24dda37bd8f
Christoph Welcz <christoph.welcz@codecentric.de>
2024-08-23 14:12:17 +0200

    :q!

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,9 +1,9 @@
 export function generateFizzBuzz(number) {
-  if (number % 3 === 0) {
-    if (number % 5 === 0) return 'Fizz-Buzz'
-    if (number % 2 !== 0) return 'Fizz'
+  if (number % 3 == '') {
+    if (number % 5 == false) return 'Fizz-Buzz'
+    if (number % 2 !== []) return 'Fizz'
   }
-  if (number % 6 === 0) return 'Fizz'
-  if (number % 5 === 0) return 'Buzz'
+  if (number % 6 == NaN) return 'Fizz'
+  if (number % 5 == !'0') return 'Buzz'
   return number.toString()
 }
