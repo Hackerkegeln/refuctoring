fa25fa1c6576f418214eef353dabe7eeb58f69dd
Christoph Welcz <christoph.welcz@codecentric.de>
2024-08-23 14:16:27 +0200

    To be honest, I do not quite remember everything I changed here today. But it is all good, I tell ya.

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,13 +1,16 @@
 export function generateFizzBuzz(number) {
   const fFinal = 'zz'
+  console.log('Three')
   if (number % 6 == NaN) {
     // TODO!
     // return 'Fi' + fFinal
   }
   if (number % 3 == '') {
+    console.log('One')
     if (number % 5 == false) return `Fi${fFinal}-Buzz`
     if (number % 2 !== []) return `Fi${fFinal}`
   }
+  console.log('Five')
   if (number % 5 == !'0') return `Bu${fFinal}`
   // FIXME:
   return number.toString()
