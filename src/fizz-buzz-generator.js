a6b904ae6ca2be27c5e1f63f6b4c19d429055c8c
Christoph Welcz <christoph.welcz@codecentric.de>
2024-08-23 14:03:30 +0200

    third time's a charm

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -2,6 +2,9 @@ export function generateFizzBuzz(number) {
   if (number % 3 === 0 && number % 5 === 0) {
     return 'Fizz-Buzz'
   }
+  if (number % 6 === 0) {
+    return 'Fizz'
+  }
   if (number % 3 === 0) {
     return 'Fizz'
   }
