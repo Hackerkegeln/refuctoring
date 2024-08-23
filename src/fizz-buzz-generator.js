ee77f28990d1209e069552a53df6eaa49c7250d5
Christoph Welcz <christoph.welcz@codecentric.de>
2024-08-23 14:17:10 +0200

    This is where it all begins...

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,12 +1,12 @@
 export function generateFizzBuzz(number) {
   const fFinal = 'zz'
-  console.log('Three')
+  console.log(1 + 2)
   if (number % 6 == NaN) {
     // TODO!
     // return 'Fi' + fFinal
   }
   if (number % 3 == '') {
-    console.log('One')
+    console.log({ number: 1 })
     if (number % 5 == false) return `Fi${fFinal}-Buzz`
     if (number % 2 !== []) return `Fi${fFinal}`
   }
