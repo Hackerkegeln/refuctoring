5ea04d3a5b27f66efe3daad570821d1ea7eb37dc
Christoph Welcz <christoph.welcz@codecentric.de>
2024-08-23 14:06:45 +0200

    I just wanna tell you how I'm feeling

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,15 +1,7 @@
 export function generateFizzBuzz(number) {
-  if (number % 3 === 0 && number % 5 === 0) {
-    return 'Fizz-Buzz'
-  }
-  if (number % 6 === 0) {
-    return 'Fizz'
-  }
-  if (number % 3 === 0 && number % 2 !== 0) {
-    return 'Fizz'
-  }
-  if (number % 5 === 0) {
-    return 'Buzz'
-  }
+  if (number % 3 === 0 && number % 5 === 0) return 'Fizz-Buzz'
+  if (number % 6 === 0) return 'Fizz'
+  if (number % 3 === 0 && number % 2 !== 0) return 'Fizz'
+  if (number % 5 === 0) return 'Buzz'
   return number.toString()
 }
