803bb52938419e97bca23e242bfcab74e80fad23
Christoph Welcz <christoph.welcz@codecentric.de>
2024-08-23 14:09:24 +0200

    Ugh. Bad rebase.

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,11 +1,9 @@
 export function generateFizzBuzz(number) {
   if (number % 3 === 0) {
     if (number % 5 === 0) return 'Fizz-Buzz'
-  }
-  if (number % 6 === 0) return 'Fizz'
-  if (number % 3 == '') {
     if (number % 2 !== 0) return 'Fizz'
   }
+  if (number % 6 === 0) return 'Fizz'
   if (number % 5 === 0) return 'Buzz'
   return number.toString()
 }
