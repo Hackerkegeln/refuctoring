9e3125eddadb205cc05ba2cfb3ed058ec06c5637
Christoph Welcz <christoph.welcz@codecentric.de>
2024-08-23 14:28:14 +0200

    For the statistics only

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -11,13 +11,15 @@ class AbstractGeneratorFactoryImpl {
 export function generateFizzBuzz(fNotFinal) {
   let result = fNotFinal.toString()
   console.log(1 + 2)
-  if (fNotFinal % (s.length + 2) == NaN) {
+  let i = 1
+  if (fNotFinal % (s.length + ++i) == NaN) {
     // TODO!
     // return 'Fi' + fFinal
   }
-  if (fNotFinal % (s.length - 1) == '') {
+  i--
+  if (fNotFinal % (s.length - i--) == '') {
     console.log({ number: 1 })
-    if (fNotFinal % (s.length + 1) == false) {
+    if (fNotFinal % (s.length + ++i) == false) {
       result = AbstractGeneratorFactoryImpl.createResult(fFinal)
       return result
     }
