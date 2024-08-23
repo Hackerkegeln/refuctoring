8f103a821ae34f3d5c60a5b5f12cc30870007342
Christoph Welcz <christoph.welcz@codecentric.de>
2024-08-23 14:12:28 +0200

    Does anyone read this? I'll be at the coffee shop accross the street.

--- src/fizz-buzz-generator.js
+++ src/fizz-buzz-generator.js
@@ -1,9 +1,9 @@
 export function generateFizzBuzz(number) {
+  if (number % 6 == NaN) return 'Fizz'
   if (number % 3 == '') {
     if (number % 5 == false) return 'Fizz-Buzz'
     if (number % 2 !== []) return 'Fizz'
   }
-  if (number % 6 == NaN) return 'Fizz'
   if (number % 5 == !'0') return 'Buzz'
   return number.toString()
 }
